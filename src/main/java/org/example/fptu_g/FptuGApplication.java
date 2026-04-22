4109f0a249cc75050a93016d82f0740f9dd0356c
MonoDuckY <pvietduc204@gmail.com>
2026-04-23 00:15:13 +0700

    Khởi tạo dự án FPTU_G với Spring Boot và Frontend

---
+++ src/main/java/org/example/fptu_g/FptuGApplication.java
@@ -0,0 +1,13 @@
+package org.example.fptu_g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FptuGApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FptuGApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/fptu_g/FptuGApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fptu_g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FptuGApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
